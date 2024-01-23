b338a8b4a8341297e2d881e4d6924fffcac2b3a5
bekthat <begzat.kamen@gmail.com>
2024-01-23 21:05:59 +0600

    Add listBuilder on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,60 @@
 import 'package:chat/services/auth/auth_service.dart';
 import 'package:chat/components/my_drawer.dart';
+import 'package:chat/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
-  void logout() {
-    //get auth service
-    final _auth = AuthService();
-    _auth.signOut();
-  }
+  //chat & auth service
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  // void logout() {
+  //   //get auth service
+  //   final _auth = AuthService();
+  //   _auth.signOut();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
-        actions: [
-          //logout button
-          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-        ],
+        title: const Text("Home"),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  //build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUserStream(),
+      builder: (context, snapshot) {
+        //error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading..");
+        }
+
+        //return   list view
+        return ListView(
+          children: snapshot.data!
+              .map<Widget>((userData) => _buildUserListItem)
+              .toList(),
+        );
+      },
+    );
+  }
+
+  //build individual list tile for user
+  Widget _buildUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
+    return UserTile();
   }
 }
