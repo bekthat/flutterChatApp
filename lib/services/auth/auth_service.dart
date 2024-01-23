2fee27bb72fe71a3808c8b3c6a86d50a2b27d1de
bekthat <begzat.kamen@gmail.com>
2024-01-23 21:51:11 +0600

    Upgrade authService

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,14 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   //instance of auth
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   //sign in
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {
+      //sign user in
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
+
+      //save user info it doesn`t already exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
@@ -18,8 +28,15 @@ class AuthService {
   //sign up
   Future<UserCredential> signUpWithEmailPassword(String email, password) async {
     try {
+      //create user
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      //save user info in a separate doc
+      _firestore.collection("Users").doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
@@ -31,5 +48,5 @@ class AuthService {
     return await _auth.signOut();
   }
 
-  //erros
+  //errors
 }
