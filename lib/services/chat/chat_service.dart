f4153a28203d52d3692bbbc730c40590b624bca9
bekthat <begzat.kamen@gmail.com>
2024-01-24 14:23:24 +0600

    Update chatService add message methods

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'package:chat/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -28,10 +29,35 @@ class ChatService {
     final Timestamp timestamp = Timestamp.now();
 
     //create a new message
-    
+    Message newMessage = Message(
+        senderID: currentUserID,
+        senderEmail: currentUserEmail,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp);
     //construct chat room ID for the two users
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort();
+    String chatRoomId = ids.join('_');
 
     //add new message to database
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomId)
+        .collection("message")
+        .add(newMessage.toMap());
   }
+
   //get message
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID) {
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
