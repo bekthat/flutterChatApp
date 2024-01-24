e7f4b472a15434a30baa8e747e3620eeef23b67c
bekthat <begzat.kamen@gmail.com>
2024-01-24 15:59:40 +0600

    Decorate UI

--- lib/pages/chat_pages.dart
+++ lib/pages/chat_pages.dart
@@ -1,3 +1,4 @@
+import 'package:chat/components/chat_bubble.dart';
 import 'package:chat/components/my_textfiled.dart';
 import 'package:chat/services/auth/auth_service.dart';
 import 'package:chat/services/chat/chat_service.dart';
@@ -35,7 +36,12 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(receiverEmail)),
+      appBar: AppBar(
+        title: Text(receiverEmail),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
+        elevation: 0,
+      ),
       body: Column(children: [
         //display all messages
         Expanded(
@@ -76,26 +82,57 @@ class ChatPage extends StatelessWidget {
   //build message item
   Widget _buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-    return Text(data["message"]);
+    //is current user
+    bool isCurrentUser = data['senderID'] == _authService.getCurrentUser()!.uid;
+
+    //align message to the right is the current user, otherwise left
+    var alignment =
+        isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Column(
+        crossAxisAlignment:
+            isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          ChatBubble(message: data["message"], isCurrentUser: isCurrentUser),
+        ],
+      ),
+    );
   }
 
   //build message input
   Widget _buildUserInput() {
-    return Row(
-      children: [
-        //textfiled should up most of the space
-        Expanded(
-          child: MyTextFiled(
-            controller: _messageController,
-            hintText: "Type a message",
-            obscureText: false,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50.0),
+      child: Row(
+        children: [
+          //textfiled should up most of the space
+          Expanded(
+            child: MyTextFiled(
+              controller: _messageController,
+              hintText: "Type a message",
+              obscureText: false,
+            ),
           ),
-        ),
 
-        // send button
-        IconButton(
-            onPressed: sendMessage, icon: const Icon(Icons.arrow_upward)),
-      ],
+          // send button
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.green,
+              shape: BoxShape.circle,
+            ),
+            margin: const EdgeInsets.only(right: 25),
+            child: IconButton(
+              onPressed: sendMessage,
+              icon: const Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
