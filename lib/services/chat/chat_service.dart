0c27b0cd39f1c6316226c0fcfd8d79a8a4d2056d
bekthat <begzat.kamen@gmail.com>
2024-01-24 08:49:20 +0600

    Update chatService

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService {
   //get instance firebase
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //get user stream
 
@@ -19,6 +21,17 @@ class ChatService {
   }
 
   //send message
+  Future<void> sendMessage(String receiverID, message) async {
+    //get current user info
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
 
+    //create a new message
+
+    //construct chat room ID for the two users
+
+    //add new message to database
+  }
   //get message
 }
